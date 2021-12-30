6051aba8bb2356edc340dfba8a2e9120f1839e3b
Abdulmuiz Mustapha <abdul@Abdulmuizs-MacBook-Pro.local>
2021-12-30 12:05:54 +0000

    first commit

---
+++ src/Components/WidgetSm/WidgetSm.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import "./widgetSm.scss";
+import Avatar from "./avatar-img.jpeg";
+import Visibility from "@material-ui/icons/VisibilityOutlined";
+import { userRequest } from "../../requestMethods";
+
+const WidgetSm = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await userRequest.get("users/?new=true");
+        setUsers(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        {users.map((user) => (
+          <li className="widgetSmListItem" key={user._id}>
+            <img
+              src={
+                user.img ||
+                "https://crowd-literature.eu/wp-content/uploads/2015/01/no-avatar.gif"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default WidgetSm;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./home.scss";
+import FeaturedInfo from "../../Components/Featuredinfo/Featuredinfo";
+import Chart from "../../Components/Chart/Chart";
+import { userData } from "../../Data";
+import WidgetSm from "../../Components/WidgetSm/WidgetSm";
+import WidgetLg from "../../Components/WidgetLg/WidgetLg";
+import { useEffect, useMemo, useState } from "react";
+import { userRequest } from "../../requestMethods";
+
+const Home = () => {
+  const [userStats, setUserStats] = useState([]);
+
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("/users/stats");
+        res.data.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "Active User": item.total },
+          ])
+        );
+      } catch {}
+    };
+    getStats();
+  }, [MONTHS]);
+
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        data={userStats}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
